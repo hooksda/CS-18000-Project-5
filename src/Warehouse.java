13f2db82f8ab44ac73990fd811b1d2991b34acf4
hooksda <hooksd@purdue.edu>
2018-12-06 21:53:25 -0500

    first commit

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,3 +1,5 @@
+import javax.xml.crypto.Data;
+import java.awt.image.DataBuffer;
 import java.io.File;
 import java.util.*;
 
@@ -50,12 +52,20 @@ public class Warehouse {
         String state = "";
         String zipCode = "";
         //1) load data (vehicle, packages, profits, packages shipped and primeday) from files using DatabaseManager
-
+        ArrayList<Package> packages = DatabaseManager.loadPackages(PACKAGE_FILE);
+        ArrayList<Vehicle> vehicles = DatabaseManager.loadVehicles(VEHICLE_FILE);
+        Double profitingOffSlaveLabor = DatabaseManager.loadProfit(PROFIT_FILE);
+        int packagesShipped = DatabaseManager.loadPackagesShipped(N_PACKAGES_FILE);
+        Boolean primedayYeet = DatabaseManager.loadPrimeDay(PRIME_DAY_FILE);
 
         //2) Show menu and handle user inputs
         asdf:
         while (prime) {
-            System.out.println(firstMenu);
+            if (primedayYeet) {
+                System.out.println(firstMenu);
+            } else {
+                System.out.println(secondMenu);
+            }
             switch (s.nextInt()) {
                 case 1:
                     System.out.println("Enter Package ID:");
@@ -82,6 +92,7 @@ public class Warehouse {
                             Integer.parseInt(zipCode));
                     Package packed = new Package(packageID, productName, Double.parseDouble(weight),
                             Double.parseDouble(price), destination);
+                    packages.add(packed);
                     packed.shippingLabel();
                     break;
                 case 2:
@@ -89,6 +100,10 @@ public class Warehouse {
                             "1) Truck\n" +
                             "2) Drone\n" +
                             "3) Cargo Plane");
+                    /**
+                     * TODO;
+                     * This while loop is not infinite must be fixed;
+                     */
                     while (true) {
                         switch (s.nextInt()) {
                             case 1:
@@ -96,18 +111,24 @@ public class Warehouse {
                                 String licensePlate = s.nextLine();
                                 System.out.println("Enter Maximum Carry Weight:");
                                 int carryWeight = s.nextInt();
+                                Truck truck = new Truck(licensePlate, carryWeight);
+                                vehicles.add(truck);
                                 break;
                             case 2:
                                 System.out.println("Enter License Plate No.:");
                                 licensePlate = s.nextLine();
                                 System.out.println("Enter Maximum Carry Weight:");
                                 carryWeight = s.nextInt();
+                                Drone drone = new Drone(licensePlate, carryWeight);
+                                vehicles.add(drone);
                                 break;
                             case 3:
                                 System.out.println("Enter License Plate No.:");
                                 licensePlate = s.nextLine();
                                 System.out.println("Enter Maximum Carry Weight:");
                                 carryWeight = s.nextInt();
+                                CargoPlane cargoPlane = new CargoPlane(licensePlate, carryWeight);
+                                vehicles.add(cargoPlane);
                                 break;
                             default:
                                 System.out.println(errorMessage);
@@ -119,95 +140,40 @@ public class Warehouse {
                         break;
                     }
                 case 3:
-                    prime = false;
+
                     break;
                 case 4:
+                    if (vehicles.size() == 0) {
+                        System.out.println("Error: No vehicles available.");
+                    } else if (packages.size() == 0){
+                        System.out.println("Error: No packages available.");
+                    } else {
+                        System.out.println("Options:\n" +
+                                "1) Send Truck\n" +
+                                "2) Send Drone\n" +
+                                "3) Send Cargo Plane\n" +
+                                "4) Send First Available");
+                        switch (s.nextInt()) {
+                            case 1:
+                            case 2:
+                            case 3:
+                            case 4:
+                        }
+                    }
+
                 case 5:
                 case 6:
                     break asdf;
                 default:
                     System.out.println(errorMessage);
             }
-            while (!prime) {
-                System.out.println(secondMenu);
-                switch (s.nextInt()) {
-                    case 1:
-                        System.out.println("Enter Package ID:");
-                        packageID = s.nextLine();
-                        System.out.println("Enter Product Name:");
-                        productName = s.nextLine();
-                        System.out.println("Enter weight:");
-                        weight = s.nextLine();
-                        System.out.println("Enter Price:");
-                        price = s.nextLine();
-                        System.out.println("Enter Buyer Name:");
-                        buyerName = s.nextLine();
-                        System.out.println("Enter Address:");
-                        address = s.nextLine();
-                        System.out.println("Enter City:");
-                        city = s.nextLine();
-                        System.out.println("Enter State:");
-                        state = s.nextLine();
-                        System.out.println("Enter Zip Code:");
-                        zipCode = s.nextLine();
-                        System.out.println();
-                        System.out.println();
-                        ShippingAddress destination = new ShippingAddress(buyerName, address, city, state,
-                                Integer.parseInt(zipCode));
-                        Package packed = new Package(packageID, productName, Double.parseDouble(weight),
-                                Double.parseDouble(price), destination);
-                        packed.shippingLabel();
-                        break;
-                    case 2:
-                        System.out.println("Vehicle Options: \n" +
-                                "1) Truck\n" +
-                                "2) Drone\n" +
-                                "3) Cargo Plane");
-                        while (true) {
-                            switch (s.nextInt()) {
-                                case 1:
-                                    System.out.println("Enter License Plate No.:");
-                                    String licensePlate = s.nextLine();
-                                    System.out.println("Enter Maximum Carry Weight:");
-                                    int carryWeight = s.nextInt();
-                                    break;
-                                case 2:
-                                    System.out.println("Enter License Plate No.:");
-                                    licensePlate = s.nextLine();
-                                    System.out.println("Enter Maximum Carry Weight:");
-                                    carryWeight = s.nextInt();
-                                    break;
-                                case 3:
-                                    System.out.println("Enter License Plate No.:");
-                                    licensePlate = s.nextLine();
-                                    System.out.println("Enter Maximum Carry Weight:");
-                                    carryWeight = s.nextInt();
-                                    break;
-                                default:
-                                    System.out.println(errorMessage);
-                                    System.out.println("Vehicle Options: \n" +
-                                            "1) Truck\n" +
-                                            "2) Drone\n" +
-                                            "3) Cargo Plane");
-                            }
-                            break;
-                        }
-                    case 3:
-                        prime = true;
-                        break;
-                    case 4:
-                    case 5:
-                    case 6:
-                        break asdf;
-                    default:
-                        System.out.println(errorMessage);
-                }
-            }
 
 
             //3) save data (vehicle, packages, profits, packages shipped and primeday) to files (overwriting them) using DatabaseManager
-
-
+            DatabaseManager.saveVehicles(VEHICLE_FILE, vehicles);
+            DatabaseManager.savePackages(PACKAGE_FILE, packages);
+            DatabaseManager.savePackagesShipped(N_PACKAGES_FILE, packagesShipped);
+            DatabaseManager.saveProfit(PROFIT_FILE, profitingOffSlaveLabor);
         }
 
     }
