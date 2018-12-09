8e18241b1b0c8fd0771ba8f60e9624f97fb878de
lee2968 <lee2968@purdue.edu>
2018-12-08 23:42:34 -0500

    hey

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -13,9 +13,6 @@ public class Vehicle {
     private ArrayList<Package> packages;
 
 
-    /**
-     * Default Constructor
-     */
     public Vehicle() {
         licensePlate = "";
         maxWeight = 0.0;
@@ -25,12 +22,6 @@ public class Vehicle {
         maximumRange = 0;
     }
 
-    /**
-     * Constructor
-     *
-     * @param licensePlate license plate of vehicle
-     * @param maxWeight    maximum weight of vehicle
-     */
     public Vehicle(String licensePlate, double maxWeight) {
         this.licensePlate = licensePlate;
         this.maxWeight = maxWeight;
@@ -40,81 +31,41 @@ public class Vehicle {
         maximumRange = 0;
     }
 
-    /**
-     * Returns the license plate of this vehicle
-     *
-     * @return license plate of this vehicle
-     */
     public String getLicensePlate() {
         return licensePlate;
     }
 
 
-    /**
-     * Updates the license plate of vehicle
-     *
-     * @param licensePlate license plate to be updated to
-     */
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
     }
 
 
-    /**
-     * Returns the maximum weight this vehicle can carry
-     *
-     * @return the maximum weight that this vehicle can carry
-     */
     public double getMaxWeight() {
         return maxWeight;
     }
 
 
-    /**
-     * Updates the maximum weight of this vehicle
-     *
-     * @param maxWeight max weight to be updated to
-     */
     public void setMaxWeight(double maxWeight) {
         this.maxWeight = maxWeight;
     }
 
 
-    /**
-     * Returns the current weight of all packages inside vehicle
-     *
-     * @return current weight of all packages inside vehicle
-     */
     public double getCurrentWeight() {
         return currentWeight;
     }
 
 
-    /**
-     * Returns the current ZIP code desitnation of the vehicle
-     *
-     * @return current ZIP code destination of vehicle
-     */
     public int getZipDest() {
         return zipDest;
     }
 
 
-    /**
-     * Updates the ZIP code destination of vehicle
-     *
-     * @param zipDest ZIP code destination to be updated to
-     */
     public void setZipDest(int zipDest) {
         this.zipDest = zipDest;
     }
 
 
-    /**
-     * Returns ArrayList of packages currently in Vehicle
-     *
-     * @return ArrayList of packages in vehicle
-     */
     public ArrayList<Package> getPackages() {
         return packages;
     }
@@ -127,13 +78,7 @@ public class Vehicle {
         return maximumRange;
     }
 
-    /**
-     * Adds Package to the vehicle only if has room to carry it (adding it would not
-     * cause it to go over its maximum carry weight).
-     *
-     * @param pkg Package to add
-     * @return whether or not it was successful in adding the package
-     */
+
     public boolean addPackage(Package pkg) {
         if (getCurrentWeight() + pkg.getWeight() <= getMaxWeight()) {
             packages.add(pkg);
@@ -145,9 +90,6 @@ public class Vehicle {
     }
 
 
-    /**
-     * Clears vehicle of packages and resets its weight to zero
-     */
     public void empty() {
         for (int i = packages.size() - 1; i >= 0; i--) {
             if (packages.get(i) != null) {
@@ -158,31 +100,21 @@ public class Vehicle {
     }
 
 
-    /**
-     * Returns true if the Vehicle has reached its maximum weight load, false
-     * otherwise.
-     *
-     * @return whether or not Vehicle is full
-     */
     public boolean isFull() {
-        if (getCurrentWeight() == getMaxWeight()) {
+        /*for (int i = 0; i < packages.size(); i++) {
+            if (packages.get(i).getWeight() + currentWeight < maxWeight) {
+                return false;
+            }
+        }
+        return true;
+        */
+        if(getCurrentWeight() == getMaxWeight()) {
             return true;
         } else {
             return false;
         }
     }
 
-
-    /**
-     * Fills vehicle with packages with preference of date added and range of its
-     * destination zip code. It will iterate over the packages intially at a range
-     * of zero and fill it with as many as it can within its range without going
-     * over its maximum weight. The amount the range increases is dependent on the
-     * vehicle that is using this. This range it increases by after each iteration
-     * is by default one.
-     *
-     * @param warehousePackages List of packages to add from
-     */
     public void fill(ArrayList<Package> warehousePackages) {
         int range = 0;
         int distance = 0;
@@ -194,14 +126,26 @@ public class Vehicle {
                     if (Math.abs(distance) == range) {
                         if (addPackage(warehousePackages.get(i))) {
                             maximumRange = distance;
+                        } else {
+                            loop = false;
                         }
                     }
                 }
                 range++;
-            } else {
+            }
+            else {
                 loop = false;
             }
         }
     }
+
+    public double getProfit() {
+        return 0.0;
+    }
+
+    public String report() {
+        return "";
+    }
 }
+
 
